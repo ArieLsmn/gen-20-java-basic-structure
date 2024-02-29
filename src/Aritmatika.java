535c8947dd6ce915d046b37c39c39376c9f386a4
Aria Lesmana <arixlsmn@gmail.com>
2024-02-29 10:00:54 +0700

    Commit 3

--- src/Aritmatika.java
+++ src/Aritmatika.java
@@ -17,6 +17,7 @@ public class Aritmatika {
         System.out.println(a+" - "+b+" = "+(a-b));
         System.out.println(a+" x "+b+" = "+(a*b));
         System.out.println(a+" / "+b+" = "+(a/b));
+        System.out.println(a+" % "+b+" = "+(a%b));
     }
     public static void main(String[] args) {
 
